830b5fd2919101e5115e6098f3af81cd18b89f70
mred5683 <hansenrd5683@gmail.com>
2016-12-03 01:13:22 -0800

    add stemandleaf to repo

---
+++ StemAndLeaf/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StemAndLeaf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10]
+            { 23, 2, 3, 34, 6, 1, 24, 45, 78, 8 };
+        }
+    }
+}
